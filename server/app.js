47885681a2e51eaf7a983cbd061ab5affb67e00a
Halit Izgın <halit_izgin@outlook.com.tr>
2018-12-25 00:20:40 +0300

    subscription introduction

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
+const http = require('http');
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer, PubSub } = require('apollo-server-express');
 const { importSchema } = require('graphql-import');
 
 const resolvers = require('./graphql/resolvers/index');
@@ -10,13 +11,16 @@ const resolvers = require('./graphql/resolvers/index');
 const User = require('./models/User');
 const Snap = require('./models/Snap');
 
+const pubsub = new PubSub();
+
 const server = new ApolloServer({
     typeDefs: importSchema('./graphql/schema.graphql'),
     resolvers,
     context: ({ req }) => ({
         User,
         Snap,
-        activeUser: req.activeUser
+        pubsub,
+        activeUser: req ? req.activeUser : null
     })
 });
 
@@ -43,6 +47,10 @@ app.use(async (req, res, next) => {
 
 server.applyMiddleware({ app });
 
-app.listen({ port: 4001 }, () => {
+const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
+httpServer.listen({ port: 4001 }, () => {
     console.log(`🚀 Server ready at http://localhost:4001${server.graphqlPath}`);
+    console.log(`🚀 Subscriptions ready at http://localhost:4001${server.subscriptionsPath}`);
 });
